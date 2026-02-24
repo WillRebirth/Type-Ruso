5ac971f305e3d4bd7b9a1ac245c0265947bcd838
Nicolas Mauricio Martinez <willtekman@aol.com>
2026-02-24 00:22:04 -0300

    new words

--- index.js
+++ index.js
@@ -12,7 +12,18 @@ const palabrasEnRuso = [
     "белый", "красный", "синий", "зеленый", "желтый", "оранжевый", "фиолетовый", "серый",
     "коричневый", "розовый", "радуга", "котенок", "щенок", "игрушка", "банан", "апельсин",
     "лимон", "арбуз", "слива", "вишня", "малина", "клубника", "абрикос", "персик", "ягода",
-    "капуста", "огурец", "помидор", "морковь", "лук", "чеснок", "картофель", "баклажан", "тыква"
+    "капуста", "огурец", "помидор", "морковь", "лук", "чеснок", "картофель", "баклажан", "тыква",
+    "быть","иметь","делать","идти","ехать","говорить","сказать","знать","думать",
+    "понимать","видеть","смотреть","слушать","слышать","читать","писать","жить",
+    "работать","учиться","учить","любить","хотеть","мочь","дать","брать","помнить",
+    "забыть","ждать","искать","найти","открыть","закрыть","начать","закончить","играть",
+    "купить","продать","пить","есть","готовить","спать","вставать","сидеть","стоять",
+    "лежать","плавать","бегать","ходить","прийти","уйти",
+    "большой","маленький","хороший","плохой","новый","старый","молодой","красивый","некрасивый","сильный",
+    "слабый","быстрый","медленный","дорогой","дешёвый","горячий","холодный","тёплый","вкусный","невкусный",
+    "интересный","скучный","трудный","лёгкий","высокий","низкий","длинный","короткий","широкий","узкий",
+    "чистый","грязный","светлый","тёмный","умный","глупый","счастливый","грустный","добрый","злой",
+    "тихий","громкий","красный","синий","зелёный","жёлтый","белый","чёрный","серый","коричневый"
 ];
 
 // Array de traducciones en inglés
@@ -29,7 +40,18 @@ const traduccionesEnIngles = [
     "anillo", "pulsera", "color", "negro", "blanco", "rojo", "azul", "verde", "amarillo", "naranja",
     "morado", "gris", "marrón", "rosa", "arcoíris", "gatito", "cachorro", "juguete", "plátano",
     "naranja", "limón", "sandía", "ciruela", "cereza", "frambuesa", "fresa", "albaricoque", "melocotón",
-    "baya", "col", "pepino", "tomate", "zanahoria", "cebolla", "ajo", "patata", "berenjena", "cachaza"
+    "baya", "col", "pepino", "tomate", "zanahoria", "cebolla", "ajo", "patata", "berenjena", "cachaza",
+    "ser / estar","tener","hacer","ir (a pie)","ir (en transporte)","hablar","decir","saber","pensar",
+    "entender","ver","mirar","escuchar","oír","leer","escribir","vivir",
+    "trabajar","estudiar","enseñar","amar","querer","poder","dar","tomar","recordar",
+    "olvidar","esperar","buscar","encontrar","abrir","cerrar","empezar","terminar","jugar",
+    "comprar","vender","beber","comer","cocinar","dormir","levantarse","sentarse","estar de pie",
+    "estar acostado","nadar","correr","caminar","llegar","irse",
+    "grande","pequeño","bueno","malo","nuevo","viejo","joven","hermoso","feo","fuerte",
+    "débil","rápido","lento","caro","barato","caliente","frío","templado","sabroso","sin sabor",
+    "interesante","aburrido","difícil","fácil","alto","bajo","largo","corto","ancho","estrecho",
+    "limpio","sucio","claro","oscuro","inteligente","tonto","feliz","triste","amable","malvado",
+    "silencioso","ruidoso","rojo","azul","verde","amarillo","blanco","negro","gris","marrón"
 ];
 
 
